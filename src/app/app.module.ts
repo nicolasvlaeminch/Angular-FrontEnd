ee8dd211ffcd1e0834b8e97bbafbe2bdbebc8131
Nicolas <nicolasvlaeminch777@gmail.com>
2024-02-07 16:14:24 -0300

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    AboutComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
